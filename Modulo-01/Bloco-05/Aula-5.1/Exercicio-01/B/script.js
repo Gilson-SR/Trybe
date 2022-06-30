237116583fb0f83547217a1ba65c5f49d6bce9ff
Gilson-SR <gilsonsrodrigues87@gmail.com>
2022-06-30 11:54:21 -0300

    Exercicio01 A e B para fixar

---
+++ Modulo-01/Bloco-05/Aula-5.1/Exercicio-01/B/script.js
@@ -0,0 +1,5 @@
+const paragraph = document.getElementById("first-paragraph");
+paragraph.style.color = "red";
+
+document.getElementsByClassName('paragraph')[0].style.color = 'Blue';
+document.getElementsByTagName('h4')[0].style.color = 'red';
